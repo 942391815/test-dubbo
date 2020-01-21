51ccf7b491b10bfdf5003699ac87101fee65f3c1
qiaogu <qiaogu@missfresh.cn>
2020-01-21 14:43:07 +0800

    init

--- dubbo/src/main/java/guava/Server.java
+++ dubbo/src/main/java/guava/Server.java
@@ -3,12 +3,12 @@ package guava;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Server {
-	   public static void main(String[] args) throws Exception { 
-	        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext( 
-	                new String[]{"server.xml" }); 
-	        context.start();
-	        System. out.println("Press any key to exit." ); 
-	        System. in.read(); 
-	    }
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                new String[]{"server.xml"});
+        context.start();
+        System.out.println("Press any key to exit.");
+        System.in.read();
+    }
 
 }
